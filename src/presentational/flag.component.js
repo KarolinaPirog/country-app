906d39bfd36530b2d5fcf8388d6375068556999c
Karolina Piróg <karolina.swieca@wp.pl>
2018-08-07 13:29:09 +0200

    change bootstrap

--- src/presentational/flag.component.js
+++ src/presentational/flag.component.js
@@ -4,6 +4,6 @@ const CountryFlag = (props) => (
     <div className="country-logo-wrapper">
         <img className="country-logo" src={props.country.imageUrl} alt="country photo" />
     </div>
-);
+)
 
 export default CountryFlag;
